c7e5b76d168d0255ff0b54ee8400a0516c22fe88
mdz <mdz.dev24@gmail.com>
2019-12-18 19:12:24 +0100

    add folder 20 visibilty marker

---
+++ 20-Visibility Markers 20/index.php
@@ -0,0 +1,26 @@
+<?php
+
+//Public
+
+//Public kan9dar n3ayat 3lih mn ay blasa Ya3ni wast lclass ola mn kharaj lclass min ma bghit
+//Public par défault katkhad soit f propriété soit f lfunction
+
+
+// Protected
+
+//Protected Kan3ayat liha ghir mn class li ana declaritha fiha ola mn class li dart liha extends mn class Parent li fiha propriete avec visibilte protected
+//Bref protected kan9dar ndir ay 7aja juste wast lclass ola wast lcass li ana ka nhérité mno
+
+
+//Private
+
+
+//Private l class li kan hérité mno ma kay9darch khdam liya b function ola propriété mdécalrine private machi b7al Protected
+
+
+
+
+
+
+
+?>
